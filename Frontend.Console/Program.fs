1038e478c32bf190fab89d0c0e9642d50f2998a0
Oleksandr Novak <onovak@spscommerce.com>
2022-07-12 20:08:35 +0200

    simplify code a little bit

--- Frontend.Console/Program.fs
+++ Frontend.Console/Program.fs
@@ -3,7 +3,6 @@ open System.IO
 
 let romName = "PONG"
 
-printfn $"Loading ROM {romName}"
 
 let loadRom(filenname) : (byte[])[] = 
 
@@ -14,6 +13,9 @@ let loadRom(filenname) : (byte[])[] =
             yield reader.ReadBytes(2)
 
     } |> Seq.toArray
+
+type nible = byte
+type trible = int16 //because why not, this is just full byte + nible
 
 let rnd = Random()
 let data = loadRom(romName)
@@ -28,27 +30,26 @@ let mutable delayTimer:byte = 0uy
 let mutable soundTimer:byte = 0uy
 let mutable stackPointer:byte = 0uy
 
-let getLowNibble(octet):byte = 
+let getLowNibble(octet):nible = 
     (byte)(octet &&& (byte)0x0F)
 
-let getHightNibble(octet) = 
+let getHightNibble(octet):nible = 
     (byte)(octet >>> 4)
 
-let getNNN(octets: byte[]): int16 =
+let getNNN(octets: byte[]): trible =
     let low0 = getLowNibble(octets[0])
     BitConverter.ToInt16([| octets[1]; low0 |], 0)
 
-let splitOctet(octet) = 
+let splitOctet(octet): nible*nible = 
     let highNibble = getHightNibble(octet)
     let lowNibble = getLowNibble(octet)
 
     (highNibble, lowNibble)
 
-let op_0NNN (octets: byte[]) = 
-    let addr = getNNN(octets)/2s
+let op_0NNN (nnn: trible) = 
     stack[(int)stackPointer] <- programCounter - 1s
     stackPointer <- stackPointer + 1uy
-    programCounter <- addr
+    programCounter <- nnn
 
 let op_00EE () = 
     stackPointer <- stackPointer - 1uy
@@ -60,103 +61,76 @@ let op_00E0 () =
         for y in 0 .. display.GetLength(1) do
             display[x,y] <- false
 
-let op_1NNN (octets: byte[]) = 
-    programCounter <- getNNN(octets) / 2s
+let op_1NNN (nnn:trible) = 
+    programCounter <- nnn / 2s
 
-let op_3XNN (octets: byte[]) = 
-    let source = getLowNibble(octets[0])
-    if variables[(int)source] = octets[1] then
+let op_3XNN (x:nible, nn:byte) = 
+    if variables[(int)x] = nn then
         programCounter <- programCounter + 1s
 
-let op_4XNN (octets: byte[]) = 
-    let source = getLowNibble(octets[0])
-    if variables[(int)source] <> octets[1] then
+let op_4XNN (x:nible, nn:byte) = 
+    if variables[(int)x] <> nn then
         programCounter <- programCounter + 1s
 
-let op_5XY0 (octets: byte[]) = 
-    let source = getLowNibble(octets[0])
-    let target = getHightNibble(octets[1])
-    if variables[(int)source] = variables[(int)target] then
+let op_5XY0 (x:nible, y:nible) = 
+    if variables[(int)x] = variables[(int)y] then
         programCounter <- programCounter + 1s
 
-let op_6XNN (octets: byte[])=
-    let source = getLowNibble(octets[0])
-    variables[(int)source] <- octets[1]
+let op_6XNN (x:nible, nn:byte)=
+    variables[(int)x] <- nn
 
-let op_7XNN (octets: byte[])=
-    let source = getLowNibble(octets[0])
-    variables[(int)source] <- variables[(int)source] + octets[1]
+let op_7XNN (x:nible, nn:byte)=
+    variables[(int)x] <- variables[(int)x] + nn
 
-let op_8XY0 (octets: byte[]) = 
-    let index_to = getLowNibble(octets[0])
-    let index_from = getHightNibble(octets[1])
-    variables[(int)index_to] <- variables[(int)index_to]
+let op_8XY0 (x:nible, y:nible) = 
+    variables[(int)x] <- variables[(int)y]
 
-let op_8XY1 (octets: byte[]) = 
-    let source = (int)(getLowNibble(octets[0]))
-    let target = (int)(getHightNibble(octets[1]))
-    variables[source] <- variables[source] ||| variables[target]
+let op_8XY1 (x:nible, y:nible) = 
+    variables[(int)x] <- variables[(int)x] ||| variables[(int)y]
 
-let op_8XY2 (octets: byte[]) = 
-    let source = (int)(getLowNibble(octets[0]))
-    let target = (int)(getHightNibble(octets[1]))
-    variables[source] <- variables[source] &&& variables[target]
+let op_8XY2 (x:nible, y:nible) = 
+    variables[(int)x] <- variables[(int)x] &&& variables[(int)y]
 
-let op_8XY3 (octets: byte[]) = 
-    let source = (int)(getLowNibble(octets[0]))
-    let target = (int)(getHightNibble(octets[1]))
-    variables[source] <- variables[source] ^^^ variables[target]
+let op_8XY3 (x:nible, y:nible) = 
+    variables[(int)x] <- variables[(int)x] ^^^ variables[(int)y]
 
-let op_8XY4 (octets: byte[]) = 
-    let source = (int)(getLowNibble(octets[0]))
-    let target = (int)(getHightNibble(octets[1]))
-    variables[source] <- variables[source] + variables[target]
+let op_8XY4 (x:nible, y:nible) = 
+    variables[(int)x] <- variables[(int)x] + variables[(int)y]
 
-let op_8XY5 (octets: byte[]) = 
-    let x = (int)(getLowNibble(octets[0]))
-    let y = (int)(getHightNibble(octets[1]))
-
-    if variables[y] > variables[x] then 
+let op_8XY5 (x:nible, y:nible) = 
+    if variables[(int)y] > variables[(int)x] then 
         variables[15] <- 0uy
     else
         variables[15] <- 1uy
 
-    variables[x] <- variables[x] - variables[y]
+    variables[(int)x] <- variables[(int)x] - variables[(int)y]
 
-let op_8XY6 (octets: byte[]) = 
-    let x = (int)(getLowNibble(octets[0]))
-    
-    variables[15] <- variables[x] &&& 1uy
-    variables[x] <- variables[x] >>> 1
+let op_8XY6 (x:nible) =    
+    variables[15] <- variables[(int)x] &&& 1uy
+    variables[(int)x] <- variables[(int)x] >>> 1
 
-let op_8XY7 (octets: byte[]) = 
-    let x = (int)(getLowNibble(octets[0]))
-    let y = (int)(getHightNibble(octets[1]))
-
-    if variables[x] > variables[y] then
+let op_8XY7 (x:nible, y:nible) = 
+    if variables[(int)x] > variables[(int)y] then
         variables[15] <- 0uy
     else
         variables[15] <- 1uy
 
-    variables[x] <- variables[y] - variables[x]
+    variables[(int)x] <- variables[(int)y] - variables[(int)x]
+
+let op_8XYE (x:nible) = 
+    variables[15] <- (variables[(int)x] &&& 0x80uy) >>> 7
+    variables[(int)x] <- variables[(int)x] <<< 1
 
 
-let op_8XYE (octets: byte[]) = 
-    let x = (int)(getLowNibble(octets[0]))    
-    variables[15] <- (variables[x] &&& 0x80uy) >>> 7
-    variables[x] <- variables[x] <<< 1
+let op_ANNN (nnn:trible) = 
+    I <- nnn
 
+let op_BNNN (nnn:trible) = 
+    programCounter <- (int16)(variables[0]) + nnn
 
-let op_ANNN (octets: byte[]) = 
-    I <- getNNN(octets)
-
-let op_BNNN (octets: byte[]) = 
-    programCounter <- (int16)(variables[0]) + getNNN(octets)
-
-let op_CXNN (octets:byte[])=
+let op_CXNN (x:nible, nn:byte)=
     let random = (byte)(rnd.Next(0,256))
-    let source = (int)(getLowNibble(octets[0]))
-    variables[source] <- random &&& octets[1]
+    variables[(int)x] <- random &&& nn
 
 let op_DXYN (x:byte, y:byte, n:byte)=
 
@@ -178,57 +152,49 @@ let op_DXYN (x:byte, y:byte, n:byte)=
     else
         variables[15] <- 0uy
     
-
-
-let op_EX9E (octets:byte[]) =
-    let source = (int)(getLowNibble(octets[0]))
-    let key = inputs[source]
+let op_EX9E (x:nible) =
+    let key = inputs[(int)x]
     if key then
         programCounter <- programCounter+1s
 
-let op_EXA1 (octets:byte[]) =
-    let source = (int)(getLowNibble(octets[0]))
-    let key = inputs[source]
+let op_EXA1 (x:nible) =
+    let key = inputs[(int)x]
     if not key then
         programCounter <- programCounter+1s
 
-let op_FX07 (octets: byte[]) = 
-    let x = (int)(getLowNibble(octets[0]))
-    variables[x] <- delayTimer
+let op_FX07 (x:nible) = 
+    variables[(int)x] <- delayTimer
 
-let op_FX0A (octets:byte[]) = 
-    let x = (int)(getLowNibble(octets[0]))
-    variables[x] <- (byte)(Console.ReadKey().Key)
+let op_FX0A (x:nible) = 
+    variables[(int)x] <- (byte)(Console.ReadKey().Key)
 
-let op_FX15 (x:byte) = 
+let op_FX15 (x:nible) = 
     delayTimer <- x
 
-let op_FX18 (x:byte) = 
+let op_FX18 (x:nible) = 
     soundTimer <- x
 
-let op_FX1E (x:byte) = 
+let op_FX1E (x:nible) = 
     I <- I + (int16)(variables[(int)x])
 
-let op_FX33 (x:byte)=
+let op_FX33 (x:nible)=
     let value = variables[(int)x]
 
     memory[(int)I] <- value / 100uy
     memory[(int)(I+(int16)1uy)] <- (value % 100uy) / 10uy
     memory[(int)(I+(int16)2uy)] <- (value % 100uy) % 10uy
 
-let op_FX55 (x:byte) =
+let op_FX55 (x:nible) =
     for i in 0uy .. x do
         memory[(int)(I+(int16)i)] <- variables[(int)i]
 
-let op_FX65 (x:byte) =
+let op_FX65 (x:nible) =
     for i in 0uy .. x do
         variables[(int)i] <- memory[(int)(I+(int16)i)]
 
 while true do
 
     let currentCommand = data.[(int)programCounter]
-
-    printfn $"{programCounter}: [0x{currentCommand[0]:X2} 0x{currentCommand[1]:X2}]"
 
     programCounter <- programCounter + 1s
 
@@ -239,31 +205,31 @@ while true do
         match (firstHigh, firstLow, secondHigh, secondLow) with
             | (0uy ,0uy, 0x0Euy, 0uy) -> op_00E0()
             | (0uy, 0uy, 0x0Euy, 0x0Euy) -> op_00EE()
-            | (0uy, _, _, _) -> op_0NNN(currentCommand)
-            | (1uy, _, _, _) -> op_1NNN(currentCommand)
-            | (2uy, _, _, _) -> op_0NNN(currentCommand)
-            | (3uy, _, _, _) -> op_3XNN(currentCommand)
-            | (4uy, _, _, _) -> op_4XNN(currentCommand)
-            | (5uy, _, _, _) -> op_5XY0(currentCommand)
-            | (6uy, _, _, _) -> op_6XNN(currentCommand)
-            | (7uy, _, _, _) -> op_7XNN(currentCommand)
-            | (8uy, _, _, 0uy) -> op_8XY0(currentCommand)
-            | (8uy, _, _, 1uy) -> op_8XY1(currentCommand)
-            | (8uy, _, _, 2uy) -> op_8XY2(currentCommand)
-            | (8uy, _, _, 3uy) -> op_8XY3(currentCommand)
-            | (8uy, _, _, 4uy) -> op_8XY4(currentCommand)
-            | (8uy, _, _, 5uy) -> op_8XY5(currentCommand)
-            | (8uy, _, _, 6uy) -> op_8XY6(currentCommand)
-            | (8uy, _, _, 7uy) -> op_8XY7(currentCommand)
-            | (8uy, _, _, 0xEuy) -> op_8XYE(currentCommand)
-            | (0x0Auy, _, _, _) -> op_ANNN(currentCommand)
-            | (0x0Buy, _, _, _) -> op_BNNN(currentCommand)
-            | (0x0Cuy, _, _, _) -> op_CXNN(currentCommand)
+            | (0uy, _, _, _) -> op_0NNN(getNNN(currentCommand))
+            | (1uy, _, _, _) -> op_1NNN(getNNN(currentCommand))
+            | (2uy, _, _, _) -> op_0NNN(getNNN(currentCommand))
+            | (3uy, x, _, _) -> op_3XNN(x, currentCommand[1])
+            | (4uy, x, _, _) -> op_4XNN(x, currentCommand[1])
+            | (5uy, x, y, 0uy) -> op_5XY0(x, y)
+            | (6uy, x, _, _) -> op_6XNN(x, currentCommand[1])
+            | (7uy, x, _, _) -> op_7XNN(x, currentCommand[1])
+            | (8uy, x, y, 0uy) -> op_8XY0(x,y)
+            | (8uy, x, y, 1uy) -> op_8XY1(x,y)
+            | (8uy, x, y, 2uy) -> op_8XY2(x,y)
+            | (8uy, x, y, 3uy) -> op_8XY3(x,y)
+            | (8uy, x, y, 4uy) -> op_8XY4(x,y)
+            | (8uy, x, y, 5uy) -> op_8XY5(x,y)
+            | (8uy, x, _, 6uy) -> op_8XY6(x)
+            | (8uy, x, y, 7uy) -> op_8XY7(x,y)
+            | (8uy, x, _, 0xEuy) -> op_8XYE(x)
+            | (0x0Auy, _, _, _) -> op_ANNN(getNNN(currentCommand))
+            | (0x0Buy, _, _, _) -> op_BNNN(getNNN(currentCommand))
+            | (0x0Cuy, x, _, _) -> op_CXNN(x, currentCommand[1])
             | (0x0Duy, x, y, n) -> op_DXYN(x,y,n)
-            | (0x0Euy, _, 9uy, 0x0Euy) -> op_EX9E(currentCommand)
-            | (0x0Euy, _, 0x0Auy, 1uy) -> op_EXA1(currentCommand)
-            | (0x0Fuy, _, 0uy, 7uy) -> op_FX07(currentCommand)
-            | (0x0Fuy, _, 0uy, 0x0Auy) -> op_FX0A(currentCommand)
+            | (0x0Euy, x, 9uy, 0x0Euy) -> op_EX9E(x)
+            | (0x0Euy, x, 0x0Auy, 1uy) -> op_EXA1(x)
+            | (0x0Fuy, x, 0uy, 7uy) -> op_FX07(x)
+            | (0x0Fuy, x, 0uy, 0x0Auy) -> op_FX0A(x)
             | (0x0Fuy, x, 1uy, 5uy) -> op_FX15(x)
             | (0x0Fuy, x, 1uy, 8uy) -> op_FX18(x)
             | (0x0Fuy, x, 1uy, 0x0Euy) -> op_FX1E(x)
@@ -274,7 +240,6 @@ while true do
     with 
         | ex -> 
             let message = $"Unable to process with opcode {currentCommand[0]:X2} {currentCommand[1]:X2} at position {(programCounter-1s)*2s}"
-            Console.WriteLine message
             raise (Exception(message, ex))
 
 
